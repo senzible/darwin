41aa71f451519a88054f558da5d45096dceb4f76
Hugo Nijhuis-Mekkelholt <hugo@nijmek.com>
2020-10-24 15:59:28 +0200

    first files

---
+++ mtl/mtl/mtl.h
@@ -0,0 +1,8 @@
+//
+// Created by Hugo Nijhuis-Mekkelholt on 24/10/2020.
+//
+
+#ifndef MTL_MTL_H
+#define MTL_MTL_H
+
+#endif //MTL_MTL_H
---
+++ mtl/mtl/mtl.m
@@ -0,0 +1,5 @@
+//
+// Created by Hugo Nijhuis-Mekkelholt on 24/10/2020.
+//
+
+#include "mtl.h"
