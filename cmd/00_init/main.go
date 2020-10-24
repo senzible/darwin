fdf02ddd29380bd13213a17d53b2a669b2cfb5ec
Hugo Nijhuis-Mekkelholt <hugo@nijmek.com>
2020-10-24 18:02:23 +0200

    reordered families

--- cmd/00_init/main.go
+++ cmd/00_init/main.go
@@ -13,13 +13,6 @@ func main() {
 
 	log.Printf("Metal Device Created: %s\n", d.Name())
 
-	log.Printf("MTLGPUFamilyCommon1: %t", d.SupportsFamily(mtl.MTLGPUFamilyCommon1))
-	log.Printf("MTLGPUFamilyCommon2: %t", d.SupportsFamily(mtl.MTLGPUFamilyCommon2))
-	log.Printf("MTLGPUFamilyCommon3: %t", d.SupportsFamily(mtl.MTLGPUFamilyCommon3))
-
-	log.Printf("MTLGPUFamilyMac1: %t", d.SupportsFamily(mtl.MTLGPUFamilyMac1))
-	log.Printf("MTLGPUFamilyMac2: %t", d.SupportsFamily(mtl.MTLGPUFamilyMac2))
-
 	log.Printf("MTLGPUFamilyApple1: %t", d.SupportsFamily(mtl.MTLGPUFamilyApple1))
 	log.Printf("MTLGPUFamilyApple2: %t", d.SupportsFamily(mtl.MTLGPUFamilyApple2))
 	log.Printf("MTLGPUFamilyApple3: %t", d.SupportsFamily(mtl.MTLGPUFamilyApple3))
@@ -27,6 +20,13 @@ func main() {
 	log.Printf("MTLGPUFamilyApple5: %t", d.SupportsFamily(mtl.MTLGPUFamilyApple5))
 	log.Printf("MTLGPUFamilyApple6: %t", d.SupportsFamily(mtl.MTLGPUFamilyApple6))
 	log.Printf("MTLGPUFamilyApple7: %t", d.SupportsFamily(mtl.MTLGPUFamilyApple7))
+
+	log.Printf("MTLGPUFamilyMac1: %t", d.SupportsFamily(mtl.MTLGPUFamilyMac1))
+	log.Printf("MTLGPUFamilyMac2: %t", d.SupportsFamily(mtl.MTLGPUFamilyMac2))
+
+	log.Printf("MTLGPUFamilyCommon1: %t", d.SupportsFamily(mtl.MTLGPUFamilyCommon1))
+	log.Printf("MTLGPUFamilyCommon2: %t", d.SupportsFamily(mtl.MTLGPUFamilyCommon2))
+	log.Printf("MTLGPUFamilyCommon3: %t", d.SupportsFamily(mtl.MTLGPUFamilyCommon3))
 
 	log.Printf("MTLGPUFamilyMacCatalyst1: %t", d.SupportsFamily(mtl.MTLGPUFamilyMacCatalyst1))
 	log.Printf("MTLGPUFamilyMacCatalyst2: %t", d.SupportsFamily(mtl.MTLGPUFamilyMacCatalyst2))
